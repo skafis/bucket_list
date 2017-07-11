38cf42e56c7161a21ebb4271f6656bbac9e82863
frank <franciskipchumba5@gmail.com>
2017-07-11 21:59:44 +0300

    creating the database

--- bucketlist_flask/schema.sql
+++ bucketlist_flask/schema.sql
@@ -1,6 +1,6 @@
 drop table if exists bucket_list;
 create table bucket_list(
-    id integer primary key autoincrement;
+    id integer primary key autoincrement,
     bucketlist_name text not null,
     bucketlist_description text
 );
